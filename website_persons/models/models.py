c1f0d1817761e863c7d8eaa8fa23f5105fdbf86b
Vladyslav Velychko <velyk95@gmail.com>
2018-01-02 16:56:21 -0500

    [ADD] models

--- website_persons/models/models.py
+++ website_persons/models/models.py
@@ -1,15 +1,33 @@
 # -*- coding: utf-8 -*-
 
+from dateutil.relativedelta import relativedelta
+from datetime import date, datetime
 from odoo import models, fields, api
 
-# class website_persons(models.Model):
-#     _name = 'website_persons.website_persons'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class Persons(models.Model):
+    _name = 'website.persons'
+
+    first_name = fields.Char(required=True)
+    last_name = fields.Char(required=True)
+    full_name = fields.Char(compute="_merge_first_last")
+    date_of_birth = fields.Date()
+    age = fields.Char(compute="_compute_age")
+    sex = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ])
+    company_id = fields.Many2one('res.company', required=True)
+
+    @api.depends('first_name', 'last_name')
+    def _merge_first_last(self):
+        self.full_name = self.first_name + self.last_name
+
+    @api.onchange('date_of_birth')
+    def _compute_age(self):
+        for record in self:
+            if record.date_of_birth:
+                dif = relativedelta(date.today(),
+                                    datetime.strptime(record.date_of_birth,
+                                                      "%Y-%m-%d").date())
+                record.age = str(dif.years)
